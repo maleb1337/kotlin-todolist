29e6269a68d1ca38f3ac7d696d254da6706a2075
maleb1337 <pe.tobar.m@gmail.com>
2021-05-22 00:02:49 -0400

    Combining multiple Flows

--- app/src/main/java/cl/maleb/todolist/data/TaskDao.kt
+++ app/src/main/java/cl/maleb/todolist/data/TaskDao.kt
@@ -1,6 +1,7 @@
 package cl.maleb.todolist.data
 
 import androidx.room.*
+import cl.maleb.todolist.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -15,7 +16,16 @@ interface TaskDao {
     @Delete
     suspend fun delete(task: Task)
 
-    @Query("SELECT * FROM taskTable WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    @Query("SELECT * FROM taskTable WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM taskTable WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when (sortOrder) {
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
 
 }
